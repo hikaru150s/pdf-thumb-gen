7b8b5a6b48a7ca0fec191fb8d51b7acdfeacbb05
Haikal Handamara <haikal_adha@hotmail.com>
2020-11-30 09:01:16 +0700

    remove unnecessary files

--- dist/lib.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-/**
- * Generate PNG thumbnail from PDF file
- * @param pathOrData either PDF path or loaded PDF Buffer
- * @returns Buffered PNG thumbnail
- */
-export declare function generateThumbnail(pathOrData: string | Buffer): Promise<Buffer>;
